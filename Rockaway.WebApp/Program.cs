cf3a63ab7dfb4ed45c3cd892e12fb2e457c3c34a
Jean-Sebastien Carle <29762210+jscarle@users.noreply.github.com>
2024-01-29 13:57:40 +0000

    Add controller.

--- Rockaway.WebApp/Program.cs
+++ Rockaway.WebApp/Program.cs
@@ -11,14 +11,18 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        builder.Services.AddRazorPages();
+        
         builder.Services.AddSingleton<IStatusReporter, StatusReporter>();
         var sqliteConnection = new SqliteConnection("Data Source=:memory:");
         sqliteConnection.Open();
         builder.Services.AddDbContext<RockawayDbContext>(options => options.UseSqlite(sqliteConnection));
+   
+        builder.Services.AddRazorPages();
+        
+        builder.Services.AddControllersWithViews();
 
         builder.Services.Configure<RouteOptions>(options => options.LowercaseUrls = true);
-
+        
         var app = builder.Build();
 
         if (!app.Environment.IsDevelopment())
@@ -41,6 +45,8 @@ public class Program
         app.UseAuthorization();
 
         app.MapRazorPages();
+        
+        app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
 
         app.MapGet("/status", (IStatusReporter reporter) => reporter.GetStatus());
         app.MapGet("/uptime", (IStatusReporter reporter) => reporter.GetUptime());
