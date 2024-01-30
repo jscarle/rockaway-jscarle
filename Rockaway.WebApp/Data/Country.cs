a13a3aa1781c8dd17bda5f45667f93142ab4192e
Jean-Sebastien Carle <29762210+jscarle@users.noreply.github.com>
2024-01-30 12:04:57 +0000

    Added venues.

---
+++ Rockaway.WebApp/Data/Country.cs
@@ -0,0 +1,25 @@
+﻿namespace Rockaway.WebApp.Data;
+
+public partial class Country
+{
+    private Country(string code, string name)
+    {
+        Code = code;
+        Name = name;
+    }
+
+    public string Code { get; set; }
+    public string Name { get; set; }
+
+    public static IList<Country> Iso3166List => Iso3166;
+
+    public static string GetName(string code)
+    {
+        return FromCode(code)?.Name ?? string.Empty;
+    }
+
+    public static Country? FromCode(string countryCode)
+    {
+        return Iso3166List.FirstOrDefault(c => c.Code.Equals(countryCode, StringComparison.InvariantCultureIgnoreCase));
+    }
+}
---
+++ Rockaway.WebApp/Data/CountryFlagTagHelper.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using Rockaway.WebApp.Data;
+
+namespace Rockaway.WebApp.TagHelpers;
+
+public class CountryFlagTagHelper : TagHelper
+{
+    public string IsoCode { get; set; } = default!;
+
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+        output.TagName = "img";
+        output.TagMode = TagMode.SelfClosing;
+        output.Attributes.Add("class", "country-flag");
+        var info = new FlagInfo(IsoCode);
+
+        output.Attributes.Add("src", info.Src);
+        output.Attributes.Add("title", info.Title);
+        output.Attributes.Add("alt", info.Alt);
+        output.Attributes.Add("srcset", info.SrcSet);
+    }
+
+    private class FlagInfo
+    {
+        private readonly string src;
+
+        public FlagInfo(string code)
+        {
+            var country = Country.FromCode(code);
+            src = (country?.Code ?? "unknown").ToLowerInvariant();
+            Title = country?.Name ?? $"unknown country {code}";
+        }
+
+        public string Src => $"/img/flags/1x/{src}.png";
+        public string SrcSet => $"/img/flags/2x/{src}.png 2x,/img/flags/3x/{src}.png 3x";
+        public string Alt => $"Flag of {Title}";
+        public string Title { get; }
+    }
+}
